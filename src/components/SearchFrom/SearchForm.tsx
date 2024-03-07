186ffccd5b297ed029fb7fcab14788c07be09c2f
Almas Kilichov <itsmywaytoamerica@gmail.com>
2024-03-07 10:05:39 +0500

    fix: fix search input

--- src/components/SearchFrom/SearchForm.tsx
+++ src/components/SearchFrom/SearchForm.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import "./styles.css";
 
 export function SearchForm() {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
   return (
     <div className="searchForm">
       <form>
-        <input type="text" value="Terry" />
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
       </form>
     </div>
   );
